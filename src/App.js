d729bcdfef663351c33c47d91f8974f87c3e7730
Tejasgaik2001 <98254757+Tejasgaik2001@users.noreply.github.com>
2023-08-02 15:19:25 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import React, { Component } from "react";
+import Navbar from "./Components/Navbar";
+import News from "./Components/News";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route
+            exact
+            path="/Business"
+            element={<News key="business" country="in" category="business" />}
+          ></Route>
+          <Route
+            exact
+            path="/Entertainment"
+            element={
+              <News key="entertainment" country="in" category="entertainment" />
+            }
+          ></Route>
+          <Route
+            exact
+            path="/General"
+            element={<News key="general" country="in" category="general" />}
+          ></Route>
+          <Route
+            exact
+            path="/Health"
+            element={<News key="health" country="in" category="health" />}
+          ></Route>
+          <Route
+            exact
+            path="/Science"
+            element={<News key="science" country="in" category="science" />}
+          ></Route>
+          <Route
+            path="/Sports"
+            element={<News key="sports" country="in" category="sports" />}
+          ></Route>
+          <Route
+            exact
+            path="/Technology"
+            element={
+              <News key="technology" country="in" category="technology" />
+            }
+          ></Route>
+        </Routes>
+      </Router>
     </div>
   );
-}
-
+};
 export default App;
